559185f2d9371e970eae47c06d31bbe321695e4c
Alexander Dänhardt <alexanderdanhardt@hotmail.com>
2021-12-09 16:13:37 +0100

    Uppdaterat metoder för rörelse

--- src/main/java/Frog.java
+++ src/main/java/Frog.java
@@ -1,6 +1,5 @@
 public class Frog {
-
-    public Position position;
+    private Position position;
     private char model;
 
     public Frog(Position position, char model) {
@@ -8,18 +7,18 @@ public class Frog {
         this.model = model;
     }
 
-    public int getX() {
-        return position.getX();
-    }
-    public int getY() {
-        return position.getY();
-            }
-    public void setX(int x) {
-        position.setX(x);
-    }
-    public void setY(int y) {
-        position.setY(position.getY()-y);
-    }
+//    public int getX() {
+//        return position.getX();
+//    }
+//    public int getY() {
+//        return position.getY();
+//            }
+//    public void setX(int x) {
+//        position.setX(x);
+//    }
+//    public void setY(int y) {
+//        position.setY(position.getY()-y);
+//    }
 
     public Position getPosition() {
         return position;
@@ -38,11 +37,29 @@ public class Frog {
     }
 
     public boolean hasReachedGoal() {
+        if (position.getY() == 10)
+            return true;
+        return false;
+    }
 
-//        if ()
+    public void moveUp() {
+        int currentFrogY = this.position.getY();
+        position.setY(currentFrogY - 1);
+    }
 
+    public void moveDown() {
+        int currentFrogY = this.position.getY();
+        position.setY(currentFrogY + 1);
+    }
 
-        return true;
+    public void moveRight() {
+        int currentFrogX = this.position.getX();
+        position.setX(currentFrogX + 1);
+    }
+
+    public void moveLeft() {
+        int currentFrogX = this.position.getX();
+        position.setX(currentFrogX - 1);
     }
 
 
