0f7d479b67aa596599e85499dda37575859882c5
SimonT <simon@tunstig.se>
2021-12-10 12:02:03 +0100

    Randomiserad startposition

---
+++ src/main/java/CarDirection.java
@@ -0,0 +1,5 @@
+public enum CarDirection {
+    RIGHT,
+    LEFT
+
+}
--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -1,12 +1,15 @@
 import com.googlecode.lanterna.terminal.Terminal;
 
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GameState {
     private Frog frog;
     private Car car;
 
     public GameState () {
        this.frog = new Frog(new Position(50, 50), 'F');
-        this.car = new Car(new Position(25,100),'C');
+        this.car = new Car(new Position(25,carStartPosition()),'C');
     }
 
     public Frog getFrog() {
@@ -42,5 +45,23 @@ public class GameState {
     }
 
 
+    // Returnerar true om grodan och bilen är på samma position
 
+public boolean collision() {
+
+    return frog.getPosition() == car.getPosition();
+}
+
+    // Returnerar randomiserad startposition för Car
+public int carStartPosition() {
+
+//    CarDirection[] arr={CarDirection.RIGHT, CarDirection.LEFT};
+//    Random r=new Random();
+//    int randomNumber=r.nextInt(arr.length);
+//    return (arr[randomNumber]);
+    int[] numbers = {1,100};
+    return numbers[ThreadLocalRandom.current().nextInt(1)];
+
+//
+}
 }
